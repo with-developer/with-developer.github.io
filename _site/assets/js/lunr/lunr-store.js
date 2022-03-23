8aff2b82ccb4b49fbbe769dd751f93081b69ff8a
with-developer <miki3079@naver.com>
2022-03-23 15:08:23 +0900

    commit on 'master'

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,1 @@
-var store = [{
-        "title": "[Jekyll] 블로그 포스팅하는 방법",
-        "excerpt":"tesst  ","categories": ["Blog"],
-        "tags": ["Blog","jekyll","Github","Git"],
-        "url": "/blog/test/",
-        "teaser": null
-      }]
+var store = []
